3227bada219050d2262cdc564e93b9e97d00bb2f
nmoren14 <128015117+nmoren14@users.noreply.github.com>
2023-04-11 01:53:17 -0500

    Creacion de footer (#23)
    Correcion de estilo total en la pagina
    
    Co-authored-by: Nicolas Moreno Gamez <ngamez@assist.com.co>

--- Secciones/addSeccion.php
+++ Secciones/addSeccion.php
@@ -45,11 +45,11 @@
             </div>
             <br>
             <div class="row">
-                <div class="col-4"></div>
-                <div class="col-4">
+            <div class="w-33">
+                  <div class="center">
                     <button class="btn btn-success">Adicionar</button>
                 </div>
-                <div class="col-4"></div>
+            </div> 
             </div>         
         </form>
     </div>
@@ -57,6 +57,5 @@
     
 <!-- Footer -->
 <?php include('../Productos/footer.php'); ?>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
--- Secciones/deleteSeccion.php
+++ Secciones/deleteSeccion.php
@@ -48,17 +48,16 @@
             </div>
             <br>
             <div class="row">
-                <div class="col-4"></div>
-                <div class="col-4">
+            <div class="w-33">
+                  <div class="center">
                     <button class="btn btn-success">Eliminar</button>
                 </div>
-                <div class="col-4"></div>
+            </div>
             </div>          
         </form>
     </div>
     <br>
 <!-- Footer -->
 <?php include('../Productos/footer.php'); ?>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
